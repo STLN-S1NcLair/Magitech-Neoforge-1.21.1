413cf306c7c52ffb477ddf5b7df7e1a696cbc35b
STLN-S1NcLair <mashumaro2584@gmail.com>
2025-09-17 17:37:30 +0900

    ToolMaterialNewの参照バグ修正

--- src/main/java/net/stln/magitech/MagitechRegistries.java
+++ src/main/java/net/stln/magitech/MagitechRegistries.java
@@ -5,14 +5,14 @@ import net.minecraft.resources.ResourceKey;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
 import net.neoforged.neoforge.registries.RegistryBuilder;
-import net.stln.magitech.item.tool.material.ToolMaterialNew;
+import net.stln.magitech.item.tool.material.ToolMaterial;
 import net.stln.magitech.magic.spell.Spell;
 
 public final class MagitechRegistries {
     private MagitechRegistries() {}
     
     public static final Registry<Spell> SPELL = new RegistryBuilder<>(Keys.SPELL).sync(true).create();
-    public static final Registry<ToolMaterialNew> TOOL_MATERIAL = new RegistryBuilder<>(Keys.TOOL_MATERIAL).sync(true).create();
+    public static final Registry<ToolMaterial> TOOL_MATERIAL = new RegistryBuilder<>(Keys.TOOL_MATERIAL).sync(true).create();
     
     public static void register(IEventBus eventBus) {
         eventBus.addListener((NewRegistryEvent event) -> {
@@ -26,6 +26,6 @@ public final class MagitechRegistries {
         private Keys() {}
         
         public static final ResourceKey<? extends Registry<Spell>> SPELL = ResourceKey.createRegistryKey(Magitech.id("spell"));
-        public static final ResourceKey<? extends Registry<ToolMaterialNew>> TOOL_MATERIAL = ResourceKey.createRegistryKey(Magitech.id("tool_material"));
+        public static final ResourceKey<? extends Registry<ToolMaterial>> TOOL_MATERIAL = ResourceKey.createRegistryKey(Magitech.id("tool_material"));
     }
 }
--- src/main/java/net/stln/magitech/item/component/MaterialComponentUtil.java
+++ src/main/java/net/stln/magitech/item/component/MaterialComponentUtil.java
@@ -1,10 +1,17 @@
 package net.stln.magitech.item.component;
 
 import net.minecraft.resources.ResourceLocation;
+import net.stln.magitech.MagitechRegistries;
+import net.stln.magitech.item.tool.material.ToolMaterial;
 import net.stln.magitech.item.tool.register.ToolMaterialRegister;
 
 public class MaterialComponentUtil {
     public static MaterialComponent generatefromId(ResourceLocation id) {
-        return new MaterialComponent(ToolMaterialRegister.getMaterial(id));
+        ToolMaterial material = ToolMaterialRegister.getMaterial(id);
+//        ToolMaterial material = MagitechRegistries.TOOL_MATERIAL.get(id);
+        if (material == null) {
+            throw new IllegalArgumentException("Invalid material id: " + id);
+        }
+        return new MaterialComponent(material);
     }
 }
