d5fc8ec04f052d7803234f4289e65ec4cb81e3a3
STLN-S1NcLair <mashumaro2584@gmail.com>
2025-12-10 19:19:15 +0900

    1.0.2

--- src/main/java/net/stln/magitech/magic/cooldown/CooldownUtil.java
+++ src/main/java/net/stln/magitech/magic/cooldown/CooldownUtil.java
@@ -1,6 +1,7 @@
 package net.stln.magitech.magic.cooldown;
 
 import com.google.common.collect.Table;
+import com.ibm.icu.impl.Pair;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.stln.magitech.magic.spell.Spell;
@@ -16,21 +17,29 @@ public class CooldownUtil {
             Table<Player, Spell, Cooldown> prevData = CooldownData.getPrevCooldownMap(player.level().isClientSide);
             List<Spell> spellsToAdd = new ArrayList<>();
             TableHelper.forEach(prevData, (r, spell, v) -> {
-                spellsToAdd.add(spell);
+                if (r.equals(player)) {
+                    spellsToAdd.add(spell);
+                }
             });
             for (Spell spell : spellsToAdd) {
                 CooldownData.removePrevCooldown(player, spell);
             }
             Table<Player, Spell, Cooldown> data = CooldownData.getCooldownMap(player.level().isClientSide);
+            List<Pair<Spell, Cooldown>> spellsToAdd2 = new ArrayList<>();
             List<Spell> spellsToRemove = new ArrayList<>();
             TableHelper.forEach(data, (player1, spell, cooldown) -> {
-                cooldown.setProgress(cooldown.getProgress() + 1);
-                if (cooldown.getProgress() > cooldown.getCooltime()) {
-                    spellsToRemove.add(spell);
-                } else {
-                    CooldownData.addCurrentCooldown(player, spell, cooldown);
+                if (player1.equals(player)) {
+                    cooldown.setProgress(cooldown.getProgress() + 1);
+                    if (cooldown.getProgress() > cooldown.getCooltime()) {
+                        spellsToRemove.add(spell);
+                    } else {
+                        spellsToAdd2.add(Pair.of(spell, cooldown));
+                    }
                 }
             });
+            for (Pair<Spell, Cooldown> pair : spellsToAdd2) {
+                CooldownData.addCurrentCooldown(player, pair.first, pair.second);
+            }
             for (Spell spell : spellsToRemove) {
                 CooldownData.removeCooldown(player, spell);
             }
